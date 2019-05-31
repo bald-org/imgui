1ef72f7cbb3c9fa927571d261ec1b5cf6e61952f
Łukasz Gut <blinekgames@gmail.com>
2019-05-31 17:17:32 +0200

    Added support for bengine

--- examples/imgui_impl_opengl3.cpp
+++ examples/imgui_impl_opengl3.cpp
@@ -100,7 +100,8 @@
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
 #include <GL/glew.h>    // Needs to be initialized with glewInit() in user's code
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
-#include <glad/glad.h>  // Needs to be initialized with gladLoadGL() in user's code
+//#include <glad/glad.h>  // Needs to be initialized with gladLoadGL() in user's code
+#include "../../../vendor/glad/include/glad/glad.h"
 #else
 #include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
 #endif
